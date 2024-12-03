53a8396d33795d8094ec895ef37a7898b6e05ecf
shirinmarri <shirinmarry@gmail.com>
2024-12-03 12:24:11 +0330

    first page is compeleted

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend:  {
+      fontfamily:{
+        "sansorif" : "sansorif"
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
+
+
