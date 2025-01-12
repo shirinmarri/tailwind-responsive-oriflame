06f6bd8a641807577322432039fa3ec294336f81
shirinmarri <shirinmarry@gmail.com>
2025-01-12 12:41:28 +0330

    darkmode is added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./*.html"],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
   theme: {
     extend:  {
       fontfamily:{
         "sansorif" : "sansorif"
       }
       
+      
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
 
